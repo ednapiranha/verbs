c5c2fb896f1eb1af805fbe517daccfe74804b31f
Jen Fong-Adwent <jen@ednapiranha.com>
2013-07-28 20:06:45 -0400

    go

---
+++ js/base/go.js
@@ -0,0 +1,31 @@
+define([], function() {
+  'use strict';
+
+  return {
+    'yo': {
+      'past': 'fui (I went)',
+      'present': 'voy (I go)',
+      'future': 'iré (I will go)'
+    },
+    'tú': {
+      'past': 'fuiste (you went)',
+      'present': 'vas (you go)',
+      'future': 'irás (you will go)'
+    },
+    'él/ella/usted': {
+      'past': 'fue (she/he went)',
+      'present': 'va (she/he goes)',
+      'future': 'irá (she/he will go)'
+    },
+    'nosotros': {
+      'past': 'fuimos (we went)',
+      'present': 'vamos (we go)',
+      'future': 'iremos (we will go)'
+    },
+    'ellos/ellas/ustedes': {
+      'past': 'fueron (they went',
+      'present': 'van (they go)'
+      'future': 'irán (they will go'
+    }
+  }
+});
