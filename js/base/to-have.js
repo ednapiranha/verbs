7df936d1526728866dda7e3ff68a897669426081
Jen Fong-Adwent <jen@ednapiranha.com>
2013-08-01 20:26:05 -0400

    added to have

---
+++ js/base/to-have.js
@@ -0,0 +1,31 @@
+define([], function() {
+  'use strict';
+
+  return {
+    'yo': {
+      'past': 'tuve (I had)',
+      'present': 'tengo (I have)',
+      'future': 'tendré (I will have)'
+    },
+    'tú': {
+      'past': 'tuviste (you had)',
+      'present': 'tienes (you have)',
+      'future': 'tendrás (you will have)'
+    },
+    'él/ella/usted': {
+      'past': 'tuvo (she/he had)',
+      'present': 'tiene (she/he have)',
+      'future': 'tendrá (she/he will have)'
+    },
+    'nosotros': {
+      'past': 'tuvimos (we had)',
+      'present': 'tenemos (we have)',
+      'future': 'tendremos (we will have)'
+    },
+    'ellos/ellas/ustedes': {
+      'past': 'tuvieron (they had',
+      'present': 'tienen (they have)'
+      'future': 'tendrán (they will have'
+    }
+  };
+});
