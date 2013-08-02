33a9408bc6804ec1da7816a61e362d1f11fea90b
Jen Fong-Adwent <jen@ednapiranha.com>
2013-08-01 20:31:28 -0400

    added eat

---
+++ js/base/eat.js
@@ -0,0 +1,31 @@
+define([], function() {
+  'use strict';
+
+  return {
+    'yo': {
+      'past': 'comí (I ate)',
+      'present': 'como (I eat)',
+      'future': 'comeré (I will eat)'
+    },
+    'tú': {
+      'past': 'comiste (you ate)',
+      'present': 'comes (you eat)',
+      'future': 'comerás (you will eat)'
+    },
+    'él/ella/usted': {
+      'past': 'comió (she/he ate)',
+      'present': 'come (she/he eats)',
+      'future': 'comerá (she/he will eat)'
+    },
+    'nosotros': {
+      'past': 'comimos (we ate)',
+      'present': 'comemos (we eat)',
+      'future': 'comeremos (we will eat)'
+    },
+    'ellos/ellas/ustedes': {
+      'past': 'comieron (they ate',
+      'present': 'comen (they eat)'
+      'future': 'comerán (they will eat'
+    }
+  };
+});
